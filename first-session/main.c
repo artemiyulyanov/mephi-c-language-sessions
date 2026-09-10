560620f009f480932d80a9e7867aecaee30a70e2
artemiyulyanov <artemiyulyanov.career@gmail.com>
2026-09-10 10:59:10 +0300

    feat: first session implemented

--- first-session/main.c
+++ first-session/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int reverseNumber(int number) {
+    int res = 0;
+
+    while (number > 0) {
+        int reminder = number % 10;
+        res = (res * 10) + reminder;
+
+        number /= 10;
+    }
+
+    return res;
+}
 
 int main(void) {
-    printf("Hello, darlings\n");
+    while (true) {
+        int number;
+
+        printf("Введите число: ");
+        scanf("%d\n", &number);
+
+        printf("Перевернутое число: %d\n", reverseNumber(number));
+    }
 
     return 0;
 }
